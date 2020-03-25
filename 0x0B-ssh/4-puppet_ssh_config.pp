17b5ff49b51384a62cd69b0b8b8ceebf1af69cc3
cmlesquivel <anayacamilo@yahoo.es>
2020-03-25 05:37:17 +0000

    tas k4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Client configuration file with Puppet
 include stdlib
 
-file_line { 'use a private key'
+file_line { 'use a private key':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => ' PasswordAuthentication no',
 }
 
-file_line { 'use a path to authenticate'
+file_line { 'use a path to authenticate':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => ' IdentityFile ~/.ssh/holberton',

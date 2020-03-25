3a44955dad74cdbf52f8e6e095a607726d24ee5c
cmlesquivel <anayacamilo@yahoo.es>
2020-03-25 05:32:32 +0000

    tas k4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,13 +2,13 @@
 include stdlib
 
 file_line { 'use a private key'
-    ensure => 'present',
+  ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
 
 file_line { 'use a path to authenticate'
-    ensure => 'present',
+  ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }

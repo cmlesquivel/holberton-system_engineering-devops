1f242969eb8b31427ed72af01af641b103c91938
cmlesquivel <1210@holbertonschool.com>
2020-05-04 20:41:29 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress. """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        doList = []
+        url = "https://jsonplaceholder.typicode.com/users/"
+        url_task = "{}{}/todos".format(url, sys.argv[1])
+        url_name = "{}{}/".format(url, sys.argv[1])
+        listTask = requests.get(url_task).json()
+        name = requests.get(url_name).json()["name"]
+        mystr = "is done with tasks"
+
+        for item in listTask:
+            if item["completed"] is True:
+                doList.append(item["title"])
+
+        print("{} {} ({}/{}):".format(name, mystr, len(doList), len(listTask)))
+        for i in doList:
+            print("\t {}".format(i))
+    except Exception as e:
+        print(e)

8f0bf7420447d5f8c0b3b7b368cd36527baae2b1
cmlesquivel <1210@holbertonschool.com>
2020-05-05 00:42:22 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
         username = requests.get(url_username).json()["username"]
 
         with open(nameFile, 'w', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
             for item in listTask:
                 spamwriter.writerow([id,
                                      username,

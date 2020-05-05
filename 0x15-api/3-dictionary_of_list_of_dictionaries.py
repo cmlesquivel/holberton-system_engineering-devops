a79850a395940c2436b63b6debc188797121d4a1
cmlesquivel <1210@holbertonschool.com>
2020-05-05 04:36:59 +0000

    Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Dictionary of list of dictionaries export data in json format """
+
+import requests
+import json
+
+if __name__ == "__main__":
+    try:
+        dic_json = {}
+        dict_user = {}
+        filename = "todo_all_employees.json"
+        url_task = "https://jsonplaceholder.typicode.com/todos"
+        url_user = "https://jsonplaceholder.typicode.com/users/"
+
+        listTask = requests.get(url_task).json()
+        username = requests.get(url_user).json()
+
+        for user in username:
+            dict_user[user["id"]] = user["username"]
+
+        for user in dict_user.keys():
+            doList = []
+            for item in listTask:
+                if user == item["userId"]:
+                    myDict = {}
+                    myDict["task"] = item["title"]
+                    myDict["completed"] = item["completed"]
+                    myDict["username"] = dict_user[user]
+                    doList.append(myDict)
+
+            dic_json[user] = doList
+
+        with open(filename, 'w') as file:
+            json.dump(dic_json, file)
+
+    except Exception as e:
+        pass

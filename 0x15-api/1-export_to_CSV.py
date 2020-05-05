bd1e21fff371e6cc70ad525cd74e2eaaf0673d95
cmlesquivel <1210@holbertonschool.com>
2020-05-05 04:48:59 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,14 +8,14 @@ import csv
 
 if __name__ == "__main__":
     try:
-        id = sys.argv[1]
-        nameFile = id + '.csv'
+        id = int(sys.argv[1])
+        nameFile = str(id) + '.csv'
         url = "https://jsonplaceholder.typicode.com/users/"
         url_task = "{}{}/todos".format(url, int(sys.argv[1]))
         url_username = "{}{}/".format(url, int(sys.argv[1]))
 
         listTask = requests.get(url_task).json()
-        username = requests.get(url_username).json()["username"]
+        username = requests.get(url_username).json().get("username")
 
         with open(nameFile, 'w', newline='') as csvfile:
             spamwriter = csv.writer(csvfile,
@@ -25,7 +25,7 @@ if __name__ == "__main__":
             for item in listTask:
                 spamwriter.writerow([id,
                                      username,
-                                     item["completed"],
-                                     item["title"]])
+                                     item.get("completed"),
+                                     item.get("title")])
     except Exception as e:
         pass

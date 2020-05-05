1efe1a03dd3533e15576f1f61afda627fe26b0c0
cmlesquivel <1210@holbertonschool.com>
2020-05-05 04:46:29 +0000

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
         username = requests.get(url_user).json()
 
         for user in username:
-            dict_user[user.get("id")] = user["username"]
+            dict_user[user.get("id")] = user.get("username")
 
         for user in dict_user.keys():
             doList = []
             for item in listTask:
-                if user == item["userId"]:
+                if user == item.get("userId"):
                     myDict = {}
                     myDict["task"] = item.get("title")
                     myDict["completed"] = item.get("completed")

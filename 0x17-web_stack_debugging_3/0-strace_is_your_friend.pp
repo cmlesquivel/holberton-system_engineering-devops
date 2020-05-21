eda14c371abd8137a64625787ece6fe73058ef95
cmlesquivel <1210@holbertonschool.com>
2020-05-21 00:50:42 +0000

    task Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
+# fix Strace is your friend
 exec { 'fix error':
       cwd     => '/var/www/html/',
       command => 'sed -i "s/false/true/g" "wp-config.php" && sed -i "s/phpp/php/g" "wp-settings.php"',
       path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
-
